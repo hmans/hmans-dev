17349db6b3d8c84980dbd3c615da21d6d7aa3ff8
Hendrik Mans <hendrik@mans.de>
2020-07-12 13:18:53 +0200

    Inline CSS, because it's 2020 and apparently that's acceptable now

--- .eleventy.js
+++ .eleventy.js
@@ -1,7 +1,14 @@
+const CleanCSS = require("clean-css");
+
 module.exports = function (config) {
   /* Passthroughs */
   config.addPassthroughCopy("src/css");
   config.addPassthroughCopy("src/images");
+
+  /* cssmin filter */
+  config.addFilter("cssmin", function (code) {
+    return new CleanCSS({}).minify(code).styles;
+  });
 
   /* Basic configuration */
   return {
